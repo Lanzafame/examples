2ff4568a685a86bd43e160d5af14e9dfabf8e23b
Makis Maropoulos <makis@ideopod.com>
2016-04-21 06:37:33 +0300

    Add examples for SendFile and simple websocket echo

---
+++ websocket_echo/server/server.go
@@ -0,0 +1,129 @@
+package main
+
+import (
+	"flag"
+	"html/template"
+	"log"
+
+	"github.com/kataras/iris"
+
+	"github.com/kataras/iris/websocket"
+)
+
+var addr = flag.String("addr", "localhost:8080", "http service address")
+
+var echoService = websocket.New(echo) // use default options
+
+func echo(c *websocket.Conn) {
+	println("from echo handler")
+	defer c.Close()
+	for {
+		mt, message, err := c.ReadMessage()
+		if err != nil {
+			log.Println("read:", err)
+			break
+		}
+		log.Printf("recv: %s", message)
+		err = c.WriteMessage(mt, message)
+		if err != nil {
+			log.Println("write:", err)
+			break
+		}
+	}
+}
+
+func echoHTTP(ctx *iris.Context) {
+	echoService.Upgrade(ctx)
+}
+
+func homeHTTP(ctx *iris.Context) {
+	ctx.Response.Header.Set(iris.ContentType, iris.ContentHTML)
+	homeTemplate.Execute(ctx.Response.BodyWriter(), "ws://"+ctx.HostString()+"/echo")
+}
+
+func main() {
+	flag.Parse()
+	log.SetFlags(0)
+	iris.Any("/echo", echoHTTP)
+	iris.Get("/", homeHTTP)
+	println("Server is running at " + *addr)
+	log.Fatal(iris.Listen(*addr))
+}
+
+var homeTemplate = template.Must(template.New("").Parse(`
+<!DOCTYPE html>
+<head>
+<meta charset="utf-8">
+<script>
+window.addEventListener("load", function(evt) {
+
+    var output = document.getElementById("output");
+    var input = document.getElementById("input");
+    var ws;
+
+    var print = function(message) {
+        var d = document.createElement("div");
+        d.innerHTML = message;
+        output.appendChild(d);
+    };
+
+    document.getElementById("open").onclick = function(evt) {
+        if (ws) {
+            return false;
+        }
+        ws = new WebSocket("{{.}}");
+        ws.onopen = function(evt) {
+            print("OPEN");
+        }
+        ws.onclose = function(evt) {
+            print("CLOSE");
+            ws = null;
+        }
+        ws.onmessage = function(evt) {
+            print("RESPONSE: " + evt.data);
+        }
+        ws.onerror = function(evt) {
+            print("ERROR: " + evt.data);
+        }
+        return false;
+    };
+
+    document.getElementById("send").onclick = function(evt) {
+        if (!ws) {
+            return false;
+        }
+        print("SEND: " + input.value);
+        ws.send(input.value);
+        return false;
+    };
+
+    document.getElementById("close").onclick = function(evt) {
+        if (!ws) {
+            return false;
+        }
+        ws.close();
+        return false;
+    };
+
+});
+</script>
+</head>
+<body>
+<table>
+<tr><td valign="top" width="50%">
+<p>Click "Open" to create a connection to the server,
+"Send" to send a message to the server and "Close" to close the connection.
+You can change the message and send multiple times.
+<p>
+<form>
+<button id="open">Open</button>
+<button id="close">Close</button>
+<p><input id="input" type="text" value="Hello world!">
+<button id="send">Send</button>
+</form>
+</td><td valign="top" width="50%">
+<div id="output"></div>
+</td></tr></table>
+</body>
+</html>
+`))
