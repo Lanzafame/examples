327ac3b051dc872f8ec7498fa5a57f6992d3b2b6
Makis Maropoulos <makis@ideopod.com>
2016-04-25 23:30:28 +0300

    Make the subdomains_simple more readable xD

--- subdomains_simple/main.go
+++ subdomains_simple/main.go
@@ -6,28 +6,24 @@ import (
 
 func main() {
 	api := iris.New()
-	//api := iris.Custom(iris.StationOptions{Cache: false})
-	//ok it's working both cached and normal router, optimization done before listen
 
-	//the subdomains are working like parties, the only difference is that you CANNOT HAVE party of party with both of them subdomains
-	//YOU CANNOT DO THAT AND YOU MUSTN'T DO IT DIRECTLY: api.Party("admin.yourhost.com").Party("other.admin.yuorhost.com")
-	//Do that: api.Party("other.admin.yourhost.com") .... and different/new party with api.Party("admin.yourhost.com")
+	// first the subdomains.
 	admin := api.Party("admin.yourhost.com")
 	{
 		//this will only success on admin.yourhost.com/hey
 		admin.Get("/hey", func(c *iris.Context) {
-			c.Write("HEY FROM admin.omicronware.com")
+			c.Write("HEY FROM admin.yourhost.com")
 		})
 		//this will only success on admin.yourhost.com/hey2
 		admin.Get("/hey2", func(c *iris.Context) {
-			c.Write("HEY SECOND FROM admin.omicronware.com")
+			c.Write("HEY SECOND FROM admin.yourhost.com")
 		})
 	}
 
-	// this will serve on yourhost.com/hey and not on admin.yourhost.com/hey
 	api.Get("/hey", func(c *iris.Context) {
 		c.Write("HEY FROM no-subdomain hey")
 	})
 
+	println("Server is listening at: 80")
 	api.Listen(":80")
 }
