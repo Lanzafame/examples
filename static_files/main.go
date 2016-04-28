68d44405595fe77b3b54ec1462d9b0a802381079
Makis Maropoulos <makis@ideopod.com>
2016-04-28 12:42:49 +0300

    add static_files example

---
+++ static_files/main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"github.com/kataras/iris"
+)
+
+type page struct {
+	Title string
+}
+
+func main() {
+
+	iris.Templates("./templates/*.html")
+
+	iris.Static("/css", "./resources/css", 1)
+	iris.Static("/js", "./resources/js", 1)
+
+	iris.Get("/", func(c *iris.Context) {
+		err := c.RenderFile("something.html", page{Title: "Home"})
+		if err != nil {
+			println(err.Error())
+		}
+	})
+
+	println("Server is running at :8080")
+	iris.Listen()
+}
