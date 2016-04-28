4cc8509a67c22110624fff441c492c9888bd1cfd
Makis Maropoulos <makis@ideopod.com>
2016-04-28 03:14:44 +0300

    Edit the sessions example in order to work with iris v1.2.0

--- sessions/main.go
+++ sessions/main.go
@@ -1,64 +1,57 @@
 package main
 
 import (
+	"time"
+
 	"github.com/kataras/iris"
 	"github.com/kataras/iris/sessions"
+
+	_ "github.com/kataras/iris/sessions/providers/memory" // add a store it is auto-registers itself
 )
 
-//there is no middleware, use the sessions anywhere you want
-func main() {
+var sess *sessions.Manager
 
-	var store = sessions.NewCookieStore([]byte("myIrisSecretKey"))
-	var mySessions = sessions.New("user_sessions", store)
+func init() {
+	sess = sessions.New("memory", "irissessionid", time.Duration(time.Minute)*60)
+}
+
+func main() {
 
 	iris.Get("/set", func(c *iris.Context) {
 		//get the session for this context
-		session, err := mySessions.Get(c) // or .GetSession(c), it's the same
+		session := sess.Start(c)
 
-		if err != nil {
-			c.WriteText(500, err.Error())
-			return
-		}
 		//set session values
 		session.Set("name", "kataras")
 
-		//save them
-		session.Save(c)
-
-		//write anthing
+		//test if setted here
 		c.Write("All ok session setted to: %s", session.Get("name"))
 	})
 
 	iris.Get("/get", func(c *iris.Context) {
-		//again get the session for this context
-		session, err := mySessions.Get(c)
+		//get the session for this context
+		session := sess.Start(c)
 
-		if err != nil {
-			c.WriteText(500, err.Error())
-			return
-		}
+		var name string
+
 		//get the session value
-		name := session.GetString("name") // .Get or .GetInt
+		if v := session.Get("name"); v != nil {
+			name = v.(string)
+		}
+		// OR just name = session.GetString("name")
 
 		c.Write("The name on the /set was: %s", name)
 	})
 
 	iris.Get("/clear", func(c *iris.Context) {
-		session, err := mySessions.Get(c)
-		if err != nil {
-			c.WriteText(500, err.Error())
-			return
-		}
-		//Clear clears all
-		//session.Clear()
+		//get the session for this context
+		session := sess.Start(c)
+
 		session.Delete("name")
 
 	})
 
-	// Use global sessions.Clear() to clear ALL sessions and stores if it's necessary
-	//sessions.Clear()
-
-	println("Iris is listening on :8080")
+	println("Server is listening at :8080")
 	iris.Listen("8080")
 
 }
