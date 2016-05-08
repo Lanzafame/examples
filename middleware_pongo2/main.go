bf308c8fd8dc79201bf86605ca6ec80b0abd6930
Makis Maropoulos <makis@ideopod.com>
2016-05-08 19:45:06 +0300

    update pongo2 example

--- middleware_pongo2/main.go
+++ middleware_pongo2/main.go
@@ -6,12 +6,15 @@ import (
 )
 
 func main() {
-	iris.Use(pongo2.Pongo2())
+	iris.Use(pongo2.Pongo2("./templates")) // or .Pongo2() defaults to "./"
+
+	iris.Config().Render.Directory = "change_this_because_its_defaults_to_templates_directory_also"
 
 	iris.Get("/", func(ctx *iris.Context) {
-		ctx.Set("template", "./thirdparty_pongo2/index.html")
-		ctx.Set("data", map[string]interface{}{"is_admin": true})
+		ctx.Set("template", "index.html")
+		ctx.Set("data", map[string]interface{}{"username": "iris", "is_admin": true})
 	})
 
+	println("Server is running at :8080")
 	iris.Listen(":8080")
 }
