0d445ffc444145bafb1d5054d65779c6c4483d17
Makis Maropoulos <makis@ideopod.com>
2016-05-12 14:34:54 +0300

    update templates example a bit

--- templates/main.go
+++ templates/main.go
@@ -12,14 +12,14 @@ type mypage struct {
 func main() {
 
 	//optionally - before the load.
-	iris.Config().Render.Delims.Left = "${" // Default is "{{"
-	iris.Config().Render.Delims.Right = "}" //this will change the behavior of {{.Property}} to ${.Property}. Default is "}}"
+	iris.Config().Render.Delims.Left = "{{"  // Default is "{{"
+	iris.Config().Render.Delims.Right = "}}" //this will change the behavior of {{.Property}} to ${.Property}. Default is "}}"
 	//iris.Config().Render.Funcs = template.FuncMap(...)
 
 	//iris.Config().Render.Directory = "templates" // Default is "templates"
-
-	iris.Config().Render.Layout = "layout" // Default is ""
-	iris.Config().Render.Gzip = true       // Default is false
+	iris.Config().Render.IsDevelopment = true // rebuild the templates on each refresh. Default is false
+	iris.Config().Render.Layout = "layout"    // Default is ""
+	iris.Config().Render.Gzip = true          // Default is false
 	iris.Get("/", func(ctx *iris.Context) {
 		ctx.Render("mypage", mypage{"My Page title", "Hello world!"}) //, "otherLayout" <- to override the layout
 	})
