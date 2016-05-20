cbbc7338e2cd9a8e39f3c80434ac5954dc80eb4d
Makis Maropoulos <makis@ideopod.com>
2016-05-20 19:24:05 +0300

    Fix typo inside pathescape Data -> Delta

--- named_parameters_pathescape/main.go
+++ named_parameters_pathescape/main.go
@@ -8,7 +8,7 @@ func main() {
 	// Request: http://localhost:8080/details/Project%2FDelta
 	// ctx.Param("project") returns the raw named parameter: Project%2FDelta
 	// which you can escape it manually with net/url: projectName, _ := url.QueryUnescape(c.Param("project"))
-	// With PathEscape = true this will redirect to 404 not found error because of the Project/Data
+	// With PathEscape = true this will redirect to 404 not found error because of the Project/Delta
 	// Look here: https://github.com/kataras/iris/issues/135
 	iris.Get("/details/:project", func(ctx *iris.Context) {
 		projectName := ctx.Param("project")
