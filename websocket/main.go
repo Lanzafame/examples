22e7b03a1a8763f41a07d12ebe59ed53281d3dcd
Makis Maropoulos <makis@ideopod.com>
2016-05-25 21:55:58 +0300

    Update the websocket example to use new rooms feature

--- websocket/main.go
+++ websocket/main.go
@@ -12,6 +12,8 @@ type clientPage struct {
 	Host  string
 }
 
+/* All commented code is working*/
+
 func main() {
 	api := iris.New()
 
@@ -25,11 +27,30 @@ func main() {
 
 	w := websocket.New(api, "/my_endpoint")
 	// for default 'iris.' station use that: w := websocket.New(iris.DefaultIris, "/my_endpoint")
-
+	var myChatRoom = "room1"
 	w.OnConnection(func(c websocket.Connection) {
+
+		/*c.On("join", func() {
+			w.Join(myChatRoom, c) // care, no c.Join, but w.Join
+		})
+
+		c.On("leave", func() {
+			w.Leave(myChatRoom, c)
+		})*/
+
+		w.Join(myChatRoom, c)
+
 		c.On("chat", func(message string) {
-			c.To(websocket.Broadcast).Emit("chat", "Message from: "+c.ID()+"-> "+message) // to all except this connection
-			c.Emit("chat", "Message from myself: "+message)
+
+			//c.To(websocket.Broadcast).Emit("chat", "Message from: "+c.ID()+"-> "+message) // to all except this connection
+			//c.Emit("chat", "Message from myself: "+message)
+
+			//send the message to the whole room, all connections are inside this room will receive this message
+			c.To(myChatRoom).Emit("chat", "From: "+c.ID()+": "+message)
+		})
+
+		c.OnDisconnect(func() {
+			fmt.Printf("\nConnection with ID: %s has been disconnected!", c.ID())
 		})
 	})
 
