398a51d6229e49a11ec287022a8a817cbfabd3c4
Makis Maropoulos <makis@ideopod.com>
2016-05-26 07:03:38 +0300

    Websocket - iris version: v3.0.0-alpha.beta | Update and create one more example

--- websocket/main.go
+++ websocket/main.go
@@ -12,40 +12,33 @@ type clientPage struct {
 	Host  string
 }
 
-/* All commented code is working*/
-
 func main() {
-	api := iris.New()
 
-	api.Static("/js", "./static/js", 1)
+	iris.Static("/js", "./static/js", 1)
 
-	api.Get("/", func(ctx *iris.Context) {
+	iris.Get("/", func(ctx *iris.Context) {
 		ctx.Render("client.html", clientPage{"Client Page", ctx.HostString()})
 	})
 
-	// important staff
+	// the path which the websocket client should listen/registed to ->
+	iris.Config().Websocket.Endpoint = "/my_endpoint"
 
-	w := websocket.New(api, "/my_endpoint")
-	// for default 'iris.' station use that: w := websocket.New(iris.DefaultIris, "/my_endpoint")
+	ws := iris.Websocket() // get the websocket server
+
 	var myChatRoom = "room1"
-	w.OnConnection(func(c websocket.Connection) {
+	ws.OnConnection(func(c websocket.Connection) {
 
-		/*c.On("join", func() {
-			w.Join(myChatRoom, c) // care, no c.Join, but w.Join
-		})
-
-		c.On("leave", func() {
-			w.Leave(myChatRoom, c)
-		})*/
-
-		w.Join(myChatRoom, c)
+		c.Join(myChatRoom)
 
 		c.On("chat", func(message string) {
+			// to all except this connection ->
+			//c.To(websocket.Broadcast).Emit("chat", "Message from: "+c.ID()+"-> "+message)
 
-			//c.To(websocket.Broadcast).Emit("chat", "Message from: "+c.ID()+"-> "+message) // to all except this connection
+			// to the client ->
 			//c.Emit("chat", "Message from myself: "+message)
 
-			//send the message to the whole room, all connections are inside this room will receive this message
+			//send the message to the whole room,
+			//all connections are inside this room will receive this message
 			c.To(myChatRoom).Emit("chat", "From: "+c.ID()+": "+message)
 		})
 
@@ -54,8 +47,6 @@ func main() {
 		})
 	})
 
-	//
-
 	fmt.Println("Server is listening at: 8080")
-	api.Listen(":8080")
+	iris.Listen(":8080")
 }
